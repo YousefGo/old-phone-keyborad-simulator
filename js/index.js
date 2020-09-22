40928574b079aebfb3c39451f046fb643666814c
yousefGo <yosuef0sarkar@gmail.com>
2020-09-22 21:08:37 +0300

    summrize to btn functions

--- js/index.js
+++ js/index.js
@@ -1,50 +1,46 @@
-var text = document.getElementById("text").innerHTML;
+var text ="";
+var m ; 
+var n =""; 
 document.getElementById("one").addEventListener("click", function() {
   randomQuote();
 
   });
   document.getElementById("two").addEventListener("click", function() {
-    pressBtn2();
-  
+   var zero ;  
+  setTimeout(() => {
+   zero =PressBtn(0);
+  }, 1500);
+  sum(zero);
   });
 
   document.getElementById("three").addEventListener("click", function() {
-    pressBtn3();
+    PressBtn(1);
   });
 
   document.getElementById("four").addEventListener("click", function() {
-    pressBtn4();
+    PressBtn(2);
   });
 
   document.getElementById("five").addEventListener("click", function() {
-    pressBtn5();
+    PressBtn(3);
   });
 
   document.getElementById("six").addEventListener("click", function() {
-    pressBtn6();
+    PressBtn(4);
   });
 
-
-
-
-
-
-
-
-
-
-
   document.getElementById("seven").addEventListener("click", function() {
-    pressBtn7();
+    PressBtn(5);
   });
 
   document.getElementById("eight").addEventListener("click", function() {
-    pressBtn8();
+    PressBtn(6);
   });
 
   document.getElementById("nine").addEventListener("click", function() {
-   var x = pressBtn9();
-  alert(x);
+ PressBtn(7);
+   
+   
   });
 
   document.getElementById("star").addEventListener("click", function() {
@@ -84,17 +80,10 @@ var btn6 = [6,'m','n','o'];
 var btn7 = [7,'p','q','r','s'];
 var btn8 = [8,'t','u','v'];
 var btn9 = [9,'w','x','y','z'];
-
-var cpt = -1;
-var cpt3=-1;
-var cpt4 = -1;
-var cpt5=-1;
-var cpt6 = -1;
-var cpt7=-1;
-var cpt8= -1;
-var cpt9=-1;
-
-
+var keypad = [btn2,btn3,btn4,
+btn5,btn6,btn7,btn8,btn9];
+var counterkey=[-1,-1,-1,-1,-1,-1,-1,-1,-1];
+cpt9=-1;
 //Init the 'par' div before click
 document.querySelector("#two").innerHTML = btn2[0];
 document.querySelector("#three").innerHTML = btn3[0];
@@ -105,146 +94,25 @@ document.querySelector("#seven").innerHTML = btn7[0];
 document.querySelector("#eight").innerHTML = btn8[0];
 document.querySelector("#nine").innerHTML = btn9[0];
 
-function pressBtn2 () 
-{
-  console.log("work");
-  if(cpt<btn2.length-1)
-  {console.log("if work");
-    cpt++;}
-  else
-   { cpt=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn2[cpt];
-}
-
-function pressBtn3 () 
-{
-  console.log("work");
-  if(cpt<btn3.length-1)
-  {console.log("if work");
-    cpt3++;}
-  else
-   { cpt=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn3[cpt3];
-}
-
-function pressBtn3 () 
-{
-  console.log("work");
-  if(cpt<btn3.length-1)
-  {console.log("if work");
-    cpt3++;}
-  else
-   { cpt=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn3[cpt3];
-}
-
-
-function pressBtn3 () 
-{
-  console.log("work");
-  if(cpt3<btn3.length-1)
-  {console.log("if work");
-    cpt3++;}
-  else
-   { cpt3=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn3[cpt3];
-}
-
-
-function pressBtn4 () 
-{
-  console.log("work");
-  if(cpt4<btn3.length-1)
-  {console.log("if work");
-    cpt4++;}
-  else
-   { cpt4=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn4[cpt4];
-}
-
-function pressBtn5 () 
-{
-  console.log("work");
-  if(cpt5<btn3.length-1)
-  {console.log("if work");
-    cpt5++;}
-  else
-   { cpt5=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn5[cpt5];
-}
-
-
-
-
-
-
-
-function pressBtn6 () 
-{
-  console.log("work");
-  if(cpt6<btn6.length-1)
-  {console.log("if work");
-    cpt6++;}
-  else
-   { cpt6=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn6[cpt6];
-}
-
-
-
-function pressBtn7 () 
-{
-  console.log("work");
-  if(cpt7<btn7.length-1)
-  {console.log("if work");
-    cpt7++;}
-  else
-   { cpt7=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn7[cpt7];
-}
-
-function pressBtn8 () 
-{
-  console.log("work");
-  if(cpt8<btn8.length-1)
-  {console.log("if work");
-    cpt8++;}
-  else
-   { cpt8=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML =text+ btn8[cpt8];
-}
-
-var m ; 
-function pressBtn9 () 
-{
- 
-  console.log("work");
-  if(cpt9<btn9.length-1)
-  {console.log("if work");
-    cpt9++;}
-  else
-   { cpt9=0;
-    console.log("else work");
-   }
-  document.querySelector("#text").innerHTML = btn9[cpt9];
-return btn9[cpt9];
+function PressBtn(key){
   
+ 
+  if(counterkey[key]<keypad[key].length-1){
+    
+    counterkey[key]=counterkey[key]+1;
+  }
+  else{
+    counterkey[key]=0;
+    console.log("f");
+  }
+  var keke = counterkey[key];
+  var h =keypad[key][keke];
+  
+ return  document.querySelector("#text").innerHTML = h;
+ 
 }
+
+function sum (key){
+text = text+key;
+}
+
